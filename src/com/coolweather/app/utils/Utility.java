1a208098952f74127863093b767d8c38e68d1434
fan <1452125537@qq.com>
2016-02-05 16:05:42 +0800

    加入显示天气的功能

--- src/com/coolweather/app/utils/Utility.java
+++ src/com/coolweather/app/utils/Utility.java
@@ -1,6 +1,15 @@
 package com.coolweather.app.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONObject;
+
 import android.R.string;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.coolweather.app.db.CoolWeatherDB;
@@ -75,9 +84,39 @@ public class Utility {
 	}
 	
 	
+	//解析服务器返回的天气数据，并且将其存储到本地
+	public static void handleWeatherResponse(Context context,String response) {
+		try {
+			JSONObject jsonObject=new JSONObject(response);
+			JSONObject weatherInfo=jsonObject.getJSONObject("weatherinfo");
+			String cityName=weatherInfo.getString("city");
+			String weatherCode=weatherInfo.getString("cityid");
+			String temp1=weatherInfo.getString("temp1");
+			String temp2=weatherInfo.getString("temp2");
+			String weatherDesp=weatherInfo.getString("weather");
+			String publishTime=weatherInfo.getString("ptime");
+			//将此数据保存起来
+			saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,publishTime);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
-	
-	
+	//将解析完成的天气信息存储到SharedPreferences文件中
+	public static void saveWeatherInfo(Context context,String cityName,String weatherCode,
+			String temp1,String temp2,String weatherDesp,String publishTime) {
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+		SharedPreferences.Editor editor=PreferenceManager.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));//将当前的日期格式化为 需要的格式
+		editor.commit();//提交
+	}
 	
 	
 	
